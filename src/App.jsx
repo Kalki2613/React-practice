bd3583459db990c6fda462de3a8e17ca47da5c28
Kallappa <Kallappahattarake@gmail.com>
2026-01-01 11:40:08 +0530

    React object-state class explained

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import LudoBoard from './LudoBoard.jsx';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <LudoBoard />
+    </>
+  )
+}
+
+export default App
